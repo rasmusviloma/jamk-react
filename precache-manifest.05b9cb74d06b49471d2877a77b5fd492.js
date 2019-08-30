a21710e8471a4847dd28f20dc33f61f332261064
Rasmus109 <Rasmus.Viloma@hotmail.com>
2019-08-30 18:41:51 +0300

    Updates

---
+++ precache-manifest.05b9cb74d06b49471d2877a77b5fd492.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af979e5b99b138a7831cf07f212389f9",
+    "url": "/jamk-react/index.html"
+  },
+  {
+    "revision": "dd15a041d90ab6977c54",
+    "url": "/jamk-react/static/css/2.0bf26eff.chunk.css"
+  },
+  {
+    "revision": "3f44bbbc61bed3d1b46d",
+    "url": "/jamk-react/static/css/main.98bd9c73.chunk.css"
+  },
+  {
+    "revision": "dd15a041d90ab6977c54",
+    "url": "/jamk-react/static/js/2.44e5402c.chunk.js"
+  },
+  {
+    "revision": "3f44bbbc61bed3d1b46d",
+    "url": "/jamk-react/static/js/main.de70c8cb.chunk.js"
+  },
+  {
+    "revision": "cb339d1adacbc157ee23",
+    "url": "/jamk-react/static/js/runtime~main.ee32fe1f.js"
+  },
+  {
+    "revision": "95f0c52b6c1e1b28f64488c61b9b5909",
+    "url": "/jamk-react/static/media/escort.95f0c52b.jpg"
+  },
+  {
+    "revision": "b0c5c5eb1e5b843ea38e52c60d8e7055",
+    "url": "/jamk-react/static/media/oswald-latin-200.b0c5c5eb.woff2"
+  },
+  {
+    "revision": "ff3daaa7c28e819b2357830f1a4cfe9f",
+    "url": "/jamk-react/static/media/oswald-latin-200.ff3daaa7.woff"
+  },
+  {
+    "revision": "30a104c5a4ea71d57872c19bdeda08a9",
+    "url": "/jamk-react/static/media/oswald-latin-300.30a104c5.woff"
+  },
+  {
+    "revision": "c5587a4685387071eb7b373b2b6ada5b",
+    "url": "/jamk-react/static/media/oswald-latin-300.c5587a46.woff2"
+  },
+  {
+    "revision": "0cf845fd83908286a7572f07eb2362c1",
+    "url": "/jamk-react/static/media/oswald-latin-400.0cf845fd.woff"
+  },
+  {
+    "revision": "45d95554b06a0ef1affa0d83bedaf0e7",
+    "url": "/jamk-react/static/media/oswald-latin-400.45d95554.woff2"
+  },
+  {
+    "revision": "e493f60b7fbdea60f10f771b91842d3f",
+    "url": "/jamk-react/static/media/oswald-latin-500.e493f60b.woff2"
+  },
+  {
+    "revision": "edf2e1854e98bae823f526834e8d4237",
+    "url": "/jamk-react/static/media/oswald-latin-500.edf2e185.woff"
+  },
+  {
+    "revision": "a5d87685024322ed56852f3b75eae890",
+    "url": "/jamk-react/static/media/oswald-latin-600.a5d87685.woff"
+  },
+  {
+    "revision": "f5860e9a357dfb5e8f4399fee7b556fd",
+    "url": "/jamk-react/static/media/oswald-latin-600.f5860e9a.woff2"
+  },
+  {
+    "revision": "4497e541fd57be36b70f08ff6e138d1f",
+    "url": "/jamk-react/static/media/oswald-latin-700.4497e541.woff2"
+  },
+  {
+    "revision": "95a794009527df11df73065e94431d33",
+    "url": "/jamk-react/static/media/oswald-latin-700.95a79400.woff"
+  },
+  {
+    "revision": "c520f5609714bc7a0dd40bc0c4f9fca3",
+    "url": "/jamk-react/static/media/passat.c520f560.jpg"
+  }
+]);
